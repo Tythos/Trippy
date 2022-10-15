cf99c0175e0d0acdf98aa112071dc5797050eadf
Brian Kirkpatrick <code@tythos.net>
2022-10-15 13:27:25 -0700

    interim commit while changing hbs reference to fork

--- Workbench/index.js
+++ Workbench/index.js
@@ -2,21 +2,37 @@
  * @author <code@tythos.net>
  */
 
-import qudom from "../qudom/index.js";
+import handlebars from "../handlebars.js/dist/handlebars.es6.js";
+
+const TEMPLATE = handlebars.compile(`<div class="{{myclass}}"/>`);
+
+function graft(parent, template, params) {
+    let div = window.document.createElement("div");
+    div.innerHTML = template(params);
+    Array.from(div.children).forEach(ch => parent.appendChild(ch));
+}
 
 /**
- * Instantiating a workbench provides UI-driven controls over window layout. This is defined by a tree of UI windows that can either be populated; split L/R (horizontally); or split T/B (vertically). These splits are resizable and each individual window comes with four controls:
+ * Instantiating a workbench provides UI-driven controls over window layout.
+ * This is defined by a tree of UI windows that can either be populated; split
+ * L/R (horizontally); or split T/B (vertically). These splits are resizable
+ * and each individual window comes with four controls:
+ * 
  * * Select/change window contents, from a known set of models
+ * 
  * * Replace with L/R split
+ * 
  * * Replace with T/B split
+ * 
  * * Close (opposing window will merge into joint space)
  * 
  * The "Workbench" instance actually handles 
  */
 class Workbench {
     constructor(parent=window.document.body) {
-        this.div = qudom("div");
-        parent.appendChild(this.div);
+        graft(parent, TEMPLATE, {
+            "myclass": "MyClass"
+        });
     }
 }
 
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import Workbench from "./Workbench/index.js";
 
 function main() {
     window.document.body.appendChild(qudom("div.Header!Some Stuff"));
+    new Workbench(window.document.body);
 }
 
 window.addEventListener("load", main);
